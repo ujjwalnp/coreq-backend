3146352fd244fa7f9c4c169f7ff238d244638a1c
ujjwalnp <dhakalujjwal99@gmail.com>
2023-06-10 15:11:40 +0545

    signup api developed

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const userRouter = require('./routes/user')
+
+// express server
+const server = express()
+
+// db connection to the database name 'coreq-backend' 
+main().catch(err => console.log(err))
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/coreq-backend')
+  console.log('database connected')
+}
+
+// body parsers
+server.use(express.json())
+server.use('/api', userRouter.router)
+
+
+// server listen port
+server.listen(8080, ()=>{
+    console.log('server started')
+})
---
+++ routes/user.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+// const userController = require("../controller/user")
+const signupController = require('../controller/signup')
+
+// app.post('/api/signup', async (req, res) => {
+
+// SIGNUP_USER API
+router.post("/signup", signupController.createUser)
+
+exports.router = router
