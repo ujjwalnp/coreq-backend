f55d4c678ce8cb99ff0e74c9c3da7d195943e1af
ujjwalnp <dhakalujjwal99@gmail.com>
2023-06-12 11:38:19 +0545

    used dotenv, 1st stage to connect mongo atlas

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
+require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
-const userRouter = require('./routes/user')
+const userRouter = require('./routes/userCredentials')
 const cors = require('cors')
 
 // express server
 const server = express()
 
-// db connection to the database name 'coreq-backend' 
+// db connection to the database name 'coreqDB' 
 main().catch(err => console.log(err))
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/coreq-backend')
+  await mongoose.connect('mongodb://127.0.0.1:27017/coreqDB')
   console.log('database connected')
 }
+
+
+// db connection to mongo atlas
+// const { MongoClient, ServerApiVersion } = require('mongodb')
+// const uri = process.env.MONGO_URL
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// })
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
 
 // body parsers
 server.use(express.json())
