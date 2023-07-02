cfcd1dc2e04cee989299dc08d8effaf3bd04e030
ujjwalnp <dhakalujjwal99@gmail.com>
2023-07-03 01:25:02 +0545

    minor changes, PORT set in .env & intalled express-session & cookie-parser

--- index.js
+++ index.js
@@ -1,35 +1,63 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
-const userRouter = require('./routes/userCredentials')
-const cors = require('cors')
+require("dotenv").config()
+const express = require("express")
+const mongoose = require("mongoose")
+const authRouter = require("./routes/auth")
+const artcileRouter = require('./routes/articles')
+const cors = require("cors")
+const session = require("express-session")
+const cookieParser = require("cookie-parser")
 
-// express server
+/* EXPRESS SERVER  */
 const server = express()
 
-// db connection to the local database name 'coreqDB' 
+/* db connection to the local database name 'coreqDB' */
 // main().catch(err => console.log(err))
 // async function main() {
 //   await mongoose.connect('mongodb://127.0.0.1:27017/coreqDB')
 //   console.log('database connected')
 // }
 
+/* DB CONNECTION TO MONGO ATLAS */
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Database Connected")
+  })
+  .catch((err) => {
+    console.log(err)
+  })
 
-// db connection to mongo atlas
-mongoose.connect(process.env.MONGO_URL)
-.then(()=>{
-  console.log('database connected')
-})
-.catch((err)=>{
-  console.log(err)
-})
-
-// body parsers
+/* BODY PARSERS */
 server.use(express.json())
-server.use('/api', cors(), userRouter.router)
 
+/* MIDDLEWARES */
 
-// server listen port
-server.listen(8080, ()=>{
-    console.log('server started')
+// sessions and cookies are still under development
+server.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      maxAge: 24 * 60 * 60 * 1000, // 24 hours
+    },
+  })
+)
+
+/* TEST API */
+server.get("/test", (req, res) => {
+  req.session.test ? req.session.test++ : (req.session.test = 1)
+  res.send(req.session.test.toString())
+});
+
+/* ROUTES */
+server.use(cors())
+server.use("/auth", authRouter.router)
+server.use('/article', artcileRouter.router)
+
+/* SERVER LISTEN */
+server.listen(process.env.SERVER_PORT, () => {
+  console.log("Server Started")
 })
