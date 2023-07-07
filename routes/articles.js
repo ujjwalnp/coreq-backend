06965414be07945752999f6e5a795d5c29b2d818
ujjwalnp <dhakalujjwal99@gmail.com>
2023-07-07 11:22:19 +0545

    routes for top articles

--- routes/articles.js
+++ routes/articles.js
@@ -3,19 +3,20 @@ const router = express.Router()
 const articleController = require('../controllers/articles')
 const { verifyToken } = require('../middlewares/auth')
 
-// READ ARTICLES
-router.get('/', verifyToken, articleController.getAllArticles)
-router.get('/:userId', verifyToken, articleController.getUserArticles)
+router
+/* READ ARTICLES */
+.get('/', verifyToken, articleController.getAllArticles)
+.get('/:userId', verifyToken, articleController.getUserArticles)
 // router.get('/:userId/articles')
 
-// CREATE ARTICLE
-router.post('/', verifyToken, articleController.createArticle)
+/* CREATE ARTICLE */
+.post('/', verifyToken, articleController.createArticle)
 
-// UPDATE ARTICLE
-router.patch('/:id/like', verifyToken, articleController.likeArticle)     // upvote & downvote
+/* UPDATE ARTICLE */
+.patch('/:id/like', verifyToken, articleController.likeArticle)     // upvote & downvote
 
-// DELETE ARTICLE
-router.delete('/:id', verifyToken, articleController.deleteArticle)
+/* DELETE ARTICLE */
+.delete('/:id', verifyToken, articleController.deleteArticle)
 
 
 exports.router = router
