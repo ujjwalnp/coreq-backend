de86382e2edcc246900156e49956b952a203f718
ujjwalnp <dhakalujjwal99@gmail.com>
2024-01-09 23:15:21 +0545

    chore(server): removed unused dependencies, add server start script, code beautified Removed dependencies: 1. cookie-parser 2. express-interceptor 3. express-session

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-require("dotenv").config()
-const express = require("express")
-const mongoose = require("mongoose")
-const authRouter = require("./routes/auth")
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./swagger-output.json')
+
+const authRouter = require('./routes/auth')
 const artcileRouter = require('./routes/articles')
 const projectRouter = require('./routes/projects')
 const archiveRouter = require('./routes/archives')
@@ -9,27 +13,15 @@ const queryRouter = require('./routes/queries')
 const usersRouter = require('./routes/users')
 const feedRouter = require('./routes/feed')
 const featureRouter = require('./routes/features')
-const cors = require("cors")
-const session = require("express-session")
-const cookieParser = require("cookie-parser")
-const swaggerUi = require('swagger-ui-express')
-const swaggerDocument = require('./swagger-output.json')
 
 /* EXPRESS SERVER  */
 const server = express()
-
-/* db connection to the local database name 'coreqDB' */
-// main().catch(err => console.log(err))
-// async function main() {
-//   await mongoose.connect('mongodb://127.0.0.1:27017/coreqDB')
-//   console.log('database connected')
-// }
 
 /* DB CONNECTION TO MONGO ATLAS */
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
-    console.log("Database Connected")
+    console.log('Database Connected')
   })
   .catch((err) => {
     console.log(err)
@@ -38,32 +30,10 @@ mongoose
 /* BODY PARSERS */
 server.use(express.json())
 
-/* MIDDLEWARES */
-
-// sessions and cookies are still under development
-server.use(
-  session({
-    secret: "keyboard cat",
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      httpOnly: true,
-      secure: false,
-      maxAge: 24 * 60 * 60 * 1000, // 24 hours
-    },
-  })
-)
-
-/* TEST API */
-server.get("/test", (req, res) => {
-  req.session.test ? req.session.test++ : (req.session.test = 1)
-  res.send(req.session.test.toString())
-});
-
 /* ROUTES */
 server.use(cors())
 server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-server.use("/auth", authRouter.router)
+server.use('/auth', authRouter.router)
 server.use('/article', artcileRouter.router)
 server.use('/project', projectRouter.router)
 server.use('/archive', archiveRouter.router)
@@ -74,5 +44,5 @@ server.use('/feature', featureRouter.router)
 
 /* SERVER LISTEN */
 server.listen(process.env.PORT, () => {
-  console.log("Server Started")
+  console.log(`Server Started on PORT ${ process.env.PORT }`)
 })
